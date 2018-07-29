45951d621f0e430d17cfa8322bda4e8f490d916b
Yuki Sumida <y.sumida.0201@gmail.com>
2018-07-29 21:18:59 +0900

    gofmt

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,23 +2,23 @@ package lexer
 
 type Lexer struct {
 	input        string
-	position     int // 入力における現在の位置
-	readPosition int // これから読み込む位置（現在の文字の次）
+	position     int  // 入力における現在の位置
+	readPosition int  // これから読み込む位置（現在の文字の次）
 	ch           byte // 現在検査中の文字
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-  l.readChar()
+	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() {
-  if l.readPosition >= len(l.input) {
-    l.ch = 0
-  } else {
-    l.ch = l.input[l.readPosition]
-  }
-  l.position = l.readPosition
-  l.readPosition += 1
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
 }
