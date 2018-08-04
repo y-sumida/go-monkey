745e85e349329c11ca93fab34e60d82085b960a7
Yuki Sumida <y.sumida.0201@gmail.com>
2018-08-04 23:25:52 +0900

    2.4構文解析器の第一歩2

---
+++ parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"go-mokey/ast"
+	"go-monkey/lexer"
+	"go-monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//2つトークンを読み込む。curTokenとpeekToken両方がセットされる
+	p.nextToken()
+	p.nextToken()
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParserProgram() *ast.Program {
+	return nil
+}
