0e5b480280046ddf4fd711a008a436c3e0fab647
Yuki Sumida <y.sumida.0201@gmail.com>
2018-08-13 21:35:49 +0900

    Fix typo.

--- ast/ast.go
+++ ast/ast.go
@@ -129,7 +129,7 @@ func (pe *PrefixExpression) expressionNode()      {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
-	out.WriteString(")")
+	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
 	out.WriteString(")")
