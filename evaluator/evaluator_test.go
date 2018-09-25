6f6004b67d321225b7c202e5c3bdf57d94b55d6f
Yuki Sumida <y.sumida.0201@gmail.com>
2018-09-25 22:48:54 +0900

    4.4.6 配列のための組み込み関数を追加する5

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -345,9 +345,12 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
 		{`len([1, 2, 3])`, 3},
+		{`len([])`, 0},
 		{`first([1, 2, 3])`, 1},
+		{`first([])`, nil},
 		{`first("string")`, "argument to `first` must be ARRAY, got STRING"},
 		{`last([1, 2, 3])`, 3},
+		{`last([])`, nil},
 		{`last("string")`, "argument to `last` must be ARRAY, got STRING"},
 		{`rest([1, 2, 3])`, []int{2, 3}},
 	}
